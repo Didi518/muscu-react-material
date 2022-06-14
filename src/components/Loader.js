966ae777107cc14dd21c4d6392f2c95c1896a56a
Didi518 <kevin.ouali@gmail.com>
2022-06-14 14:22:05 +0200

    api youtube + fin de projet

---
+++ src/components/Loader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Stack } from '@mui/material';
+import { ThreeCircles } from 'react-loader-spinner';
+
+const Loader = () => (
+  <Stack
+    direction="row"
+    justifyContent="center"
+    alignItems="center"
+    width="100%"
+  >
+    <ThreeCircles color="grey" />
+  </Stack>
+);
+
+export default Loader;
